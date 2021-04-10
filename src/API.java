ab8ded21aff181c81aed054d28446fe87b69bc50
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2021-04-10 01:56:31 -0700

    Completed API class excluding error handling
    Completed the API class to its final level of completion excluding the error handling.

--- src/API.java
+++ src/API.java
@@ -15,41 +15,119 @@
  * Github repository: https://github.com/TwitchRecover/VOD-Domain-Retriever
  */
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 /**
- * This class handles all of the API requests.
+ * This class handles all of the API and web requests.
  */
 public class API {
     //Constants:
-    private static final String TWITCH_ACCEPT="application/vnd.twitchtv.v5+json";
     private static String WEB_CI;
     private static final int HTTP_OK=200;
-    //Header constants:
-    private static final String ACCEPT="Accept";
-    private static final String CI="Client-ID";
-    //Domain query constants:
-    private static final String D="https://api.twitch.tv/top?";
-    //Query constants:
-    private static final String LIM="limit=100";
-    private static final String OFF="offset=";
-    private static final String PER="period=week";
-    //Query constants:
-    private static final String D_MAIN=D+LIM+"&"+PER;
-    //Query parameters:
-    private static String offset;
-    private static String query;
 
     /**
-     * Mutator for the offset query parameter.
-     * @param offIn Integer value representing the offset value to set.
+     * Web requests:
      */
-    private static void setOffset(int offIn){
-        offset=OFF+offIn;
+
+    /**
+     * This function returns the values of a simple
+     * get call to a given URL.
+     * @param url                   String value representing the URL to query.
+     * @return ArrayList<String>    String arraylist which contains the entire response
+     */
+    private static ArrayList<String> webReq(String url){
+        ArrayList<String> response=new ArrayList<String>();
+        try{
+            CloseableHttpClient httpClient=HttpClients.createDefault();
+            HttpGet httpGet=new HttpGet(url);
+            CloseableHttpResponse httpResponse=httpClient.execute(httpGet);
+            if(httpResponse.getStatusLine().getStatusCode()==HTTP_OK){
+                BufferedReader br=new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+                String line;
+                while((line=br.readLine())!=null){
+                    response.add(line);
+                }
+                br.close();
+            }
+            httpResponse.close();
+            httpClient.close();
+        }
+        catch(Exception ignored){}
+        return response;
     }
 
     /**
-     * Sets the final query URL.
+     * This function retrieves the web client ID of Twitch
+     * from a file in the Twitch Recover repository which tracks them.
      */
-    private static void setQuery(){
-        query=D_MAIN+"&"+offset;
+    private static void retrieveWEBCI(){
+        ArrayList<String> response=webReq("https://raw.githubusercontent.com/TwitchRecover/TwitchRecover/master/WEB_CI.txt");
+        for(String s: response){
+            if(!s.startsWith("#")){
+                WEB_CI=s;
+                return;
+            }
+        }
+        WEB_CI="kimne78kx3ncx6brgo4mv6wki5h1ko";
+    }
+
+    /**
+     * Retrieves the known domains from the Twitch Recover main repository.
+     * @return ArrayList<String>        String arraylist containing anll of the known domains.
+     */
+    protected static ArrayList<String> getDomains(){
+        ArrayList<String> domains=new ArrayList<String>();
+        ArrayList<String> response=webReq("https://raw.githubusercontent.com/TwitchRecover/TwitchRecover/master/domains.txt");
+        if(response.isEmpty()){
+            //TODO: Add error handling.
+        }
+        else{
+            domains.addAll(response);
+        }
+        return domains;
+    }
+
+    /**
+     * API Requests:
+     */
+
+    /**
+     * This function gets the list of the top 100 VODs from a
+     * given offset value and each VOD's API response.
+     * @param offset    Integer value representing the offset of the top videos to get from, in the bounds of [0,500].
+     * @return String   String value containing the entire API JSON response.
+     */
+    protected static String getVods(int offset){
+        ArrayList<String> response=new ArrayList<String>();
+        try{
+            CloseableHttpClient httpClient=HttpClients.createDefault();
+            HttpGet httpGet=new HttpGet("https://api.twitch.tv/kraken/videos/top?period=week&limit=100&offset="+offset);
+            httpGet.addHeader("Accept","application/vnd.twitchtv.v5+json");
+            httpGet.addHeader("Client-ID",WEB_CI);
+            CloseableHttpResponse httpResponse=httpClient.execute(httpGet);
+            if(httpResponse.getStatusLine().getStatusCode()==HTTP_OK){
+                BufferedReader br=new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+                String line;
+                while((line=br.readLine())!=null){
+                    response.add(line);
+                }
+                br.close();
+            }
+            else{
+                //TODO: Error handling.
+            }
+            httpResponse.close();
+            httpClient.close();
+        }
+        catch(Exception e){
+            //TODO: Error handling.
+        }
+        return String.join(", ",response);
     }
 }
