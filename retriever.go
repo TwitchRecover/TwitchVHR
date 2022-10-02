5567666e56482fec7ed1de959fa5bf443ee10bac
Daylam Tayari <git@tayari.gg>
2022-10-02 12:35:43 -0700

    Optimised retriever to remove duplicates within a single vod

--- retriever.go
+++ retriever.go
@@ -16,9 +16,18 @@ func RetrieveFeeds(vodID string) []Hosts {
 		feed := hostRe.FindStringSubmatch(f)
 		if feed == nil {
 			fmt.Errorf("Feed URL %v does not contain a valid host.", f)
-		} else {
+		} else if UniqueHost(hosts, feed[1]) {
 			hosts = append(hosts, Hosts{Name: feed[1]})
 		}
 	}
 	return hosts
+}
+
+func UniqueHost(hosts []Hosts, host string) bool {
+	for i := range hosts {
+		if hosts[i].Name == host {
+			return false
+		}
+	}
+	return true
 }
